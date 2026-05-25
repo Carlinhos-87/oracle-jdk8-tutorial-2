59e7de45b667ee0e1083fad54a0fbe8f6103f797
Carlinhos-87 <jcpapark87@gmail.com>
2026-05-25 18:13:01 +0200

    patron builder de lombok explicado

--- oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
+++ oracle-jdk8-tutorial-2/src/main/java/com/example/App.java
@@ -20,7 +20,27 @@ public class App {
 		// la variedad de la Manzana, me daría error porque yo no tengo un constructor
 		// que reciba un solo parametro
 
-		Manzana manzana2 = new Manzana("Golden");
+		/*
+		 * Utilizando lombok no necesitamos tener todos los constructores definidos,
+		 * solamente el constructor sin parametros o por defecto y el constructor que
+		 * recibe todos los campos de la clase
+		 * 
+		 * Utilizando el PATRON BUILDER DE LOMBOK, es posible construir cualquier objeto
+		 * aunque no tengas su constructor explicitamente y tampoco estariamos obligados
+		 * a suministrar los campos o propiedades del objeto en un orden determinado
+		 */
+
+		// El Patron Builder NO fue inventado por lombok, pero lo ha mejorado bastante
+
+		// El Patron Builder es patron factoria, es decir, creacional, que permite crear
+		// o instanciar objetos sin tener explicitamente su constructor
+
+		Manzana manzana2 = Manzana.builder()
+				.variedad("Golden")
+				.build();
+		/*¿Como imprimir en la consola el objeto referenciado por la variable manzana1?*/
+		
+		//Manzana manzana2 = new Manzana("Golden");
 		System.out.println("------- Imprimiendo manzana2 -------");
 		System.out.println(manzana2);
 
@@ -28,10 +48,22 @@ public class App {
 		 * ¿Cómo comparar dos objetos Manzana para saber si son iguales o no? Vamos a
 		 * crear dos objetos Manzana para que sean de la misma variedad y precio
 		 */
+		
+		//Manzana manzana3 = new Manzana("reineta", new BigDecimal(2.0));
+		
+		Manzana manzana3 = Manzana.builder()
+				.variedad("Reineta")
+				.precio(new BigDecimal(2.0))
+				.build();
+				
+		//Manzana manzana4 = new Manzana("reineta", new BigDecimal(2.0));
 
-		Manzana manzana3 = new Manzana("reineta", new BigDecimal(2.0));
-		Manzana manzana4 = new Manzana("reineta", new BigDecimal(2.0));
-
+		Manzana manzana4 = Manzana.builder()
+				.variedad("Reineta")
+				.precio(new BigDecimal(2.0))
+				.build();
+		
+		
 		int x = 5;
 		int y = 5;
 
@@ -39,7 +71,7 @@ public class App {
 		 * Si quiero comprobar si las variables x e y son iguales, podemos utilizar una
 		 * sentencia de Java que la tienen todos los lenguajes de programación,
 		 * denominada sentencia condicional if - else, que evalua una condición entre
-		 * parentesis, y que si se cumple se ejecuta el grupo de sentencias que aparecen
+		 * parentesis y que si se cumple se ejecuta el grupo de sentencias que aparecen
 		 * en el bloque encerrado entre las llaves de apertura y cierre, y si no se
 		 * cumple la condicion se podra ejecutar la o las sentencias que estan en
 		 * siguiente bloque
